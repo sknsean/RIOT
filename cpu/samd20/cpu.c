037fef6c87107322e7ffdb98d7a3fbac0bba3399
Sean Nyekjaer <sean.nyekjaer@prevas.dk>
2017-04-10 12:57:17 +0200

    samd20: dfll 48mhz run

--- cpu/samd20/cpu.c
+++ cpu/samd20/cpu.c
@@ -31,12 +31,10 @@ static void clk_init(void)
     PM->APBAMASK.reg = (PM_APBAMASK_PM | PM_APBAMASK_SYSCTRL |
                         PM_APBAMASK_GCLK);
 
-    /* adjust NVM wait states, see table 42.30 (p. 1070) in the datasheet */
-#if (CLOCK_CORECLOCK > 24000000)
+    /* adjust NVM wait states, errata 13134 */
     PM->APBAMASK.reg |= PM_AHBMASK_NVMCTRL;
     NVMCTRL->CTRLB.reg |= NVMCTRL_CTRLB_RWS(1);
     PM->APBAMASK.reg &= ~PM_AHBMASK_NVMCTRL;
-#endif
 
     /* configure internal 8MHz oscillator to run without prescaler */
     SYSCTRL->OSC8M.bit.PRESC = 0;
@@ -45,7 +43,7 @@ static void clk_init(void)
     SYSCTRL->OSC8M.bit.ENABLE = 1;
     while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_OSC8MRDY)) {}
 
-#if CLOCK_USE_PLL
+#if CLOCK_USE_DFLL
     /* reset the GCLK module so it is in a known state */
     GCLK->CTRL.reg = GCLK_CTRL_SWRST;
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
@@ -61,18 +59,35 @@ static void clk_init(void)
                          GCLK_CLKCTRL_CLKEN);
     while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY) {}
 
-    /* enable PLL */
-    SYSCTRL->DPLLRATIO.reg = (SYSCTRL_DPLLRATIO_LDR(CLOCK_PLL_MUL));
-    SYSCTRL->DPLLCTRLB.reg = (SYSCTRL_DPLLCTRLB_REFCLK_GCLK);
-    SYSCTRL->DPLLCTRLA.reg = (SYSCTRL_DPLLCTRLA_ENABLE);
-    while(!(SYSCTRL->DPLLSTATUS.reg &
-           (SYSCTRL_DPLLSTATUS_CLKRDY | SYSCTRL_DPLLSTATUS_LOCK))) {}
+    /* enable DFLL */
+#define NVM_DFLL_COARSE_POS    58 /* DFLL48M Coarse calibration value bit position.*/
+#define NVM_DFLL_COARSE_SIZE   6  /* DFLL48M Coarse calibration value bit size.*/
+#define NVM_DFLL_FINE_POS    64 /* DFLL48M Coarse calibration value bit position.*/
+#define NVM_DFLL_FINE_SIZE   10  /* DFLL48M Fine calibration value bit size.*/
+
+    /* errata_9905 */
+    SYSCTRL->DFLLCTRL.reg = SYSCTRL->DFLLCTRL.reg & ~SYSCTRL_DFLLCTRL_ONDEMAND;
+    while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY)) {}
+
+    /* Using DFLL48M COARSE CAL value from NVM Software Calibration Area Mapping
+       in DFLL.COARSE helps to output a frequency close to 48 MHz. */
+    uint32_t coarse =( *((uint32_t *)(NVMCTRL_OTP4)
+                      + (NVM_DFLL_COARSE_POS / 32))
+                      >> (NVM_DFLL_COARSE_POS % 32))
+                      & ((1 << NVM_DFLL_COARSE_SIZE) - 1);
+    uint32_t fine =( *((uint32_t *)(NVMCTRL_OTP4)
+                      + (NVM_DFLL_FINE_POS / 32))
+                      >> (NVM_DFLL_FINE_POS % 32))
+                      & ((1 << NVM_DFLL_FINE_SIZE) - 1);
+    SYSCTRL->DFLLVAL.reg = SYSCTRL_DFLLVAL_COARSE(coarse) | SYSCTRL_DFLLVAL_FINE(fine);
+    SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE;
+    while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY)) {}
 
     /* select the PLL as source for clock generator 0 (CPU core clock) */
-    GCLK->GENDIV.reg =  (GCLK_GENDIV_DIV(CLOCK_PLL_DIV) |
+    GCLK->GENDIV.reg =  (GCLK_GENDIV_DIV(CLOCK_DFLL_DIV) |
                         GCLK_GENDIV_ID(0));
     GCLK->GENCTRL.reg = (GCLK_GENCTRL_GENEN |
-                         GCLK_GENCTRL_SRC_FDPLL |
+                         GCLK_GENCTRL_SRC_DFLL48M |
                          GCLK_GENCTRL_ID(0));
 #else /* do not use PLL, use internal 8MHz oscillator directly */
     GCLK->GENDIV.reg =  (GCLK_GENDIV_DIV(CLOCK_DIV) |
@@ -94,7 +109,7 @@ static void clk_init(void)
     while (GCLK->STATUS.bit.SYNCBUSY) {}
 
     /* redirect all peripherals to a disabled clock generator (7) by default */
-    for (int i = 0x3; i <= 0x22; i++) {
+    for (int i = 0x3; i <= 0x1B; i++) {
         GCLK->CLKCTRL.reg = ( GCLK_CLKCTRL_ID(i) | GCLK_CLKCTRL_GEN_GCLK7 );
         while (GCLK->STATUS.bit.SYNCBUSY) {}
     }
