821627d1d8595edc09a493db7628ff559b13820c
Martin Hundebøll <martin@hundeboll.net>
2017-04-12 00:01:41 +0200

    boards: mksensor: initial board support based on arduiner-zero

---
+++ boards/mksensor/board.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C)  2017 Sean Nyekjaer
+ * Copyright (C)  2016 Freie Universität Berlin
+ *                2016 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_mksensor
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the mksensor board
+ *
+ * @author      Sean Nyekjaer <sean@nyekjaer.dk>
+ * @author      Hauke Pertersen  <hauke.pertersen@fu-berlin.de>
+ * @author      Alexandre Abadie <alexandre.abadie@inria.fr>
+ *
+ * @}
+ */
+
+#include "cpu.h"
+#include "board.h"
+#include "periph/gpio.h"
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+
+    /* initialize the on-board Amber "L" LED on pin PA17 / PORT A25 */
+    gpio_init(LED0_PIN, GPIO_OUT);
+
+    gpio_set(LED0_PIN);
+    gpio_clear(LED0_PIN);
+}
+
+void leds_toggle(void)
+{
+    gpio_toggle(LED0_PIN);
+}
